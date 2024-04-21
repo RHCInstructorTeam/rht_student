88dd314888182ec8ce6850e839785ccdb402ddba
Travis Michette <tmichett@redhat.com>
2024-04-21 15:05:37 -0400

    Added VIMRC Configuration

---
+++ vimrc_config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#
+# Travis Michette <tmichett@redhat.com>
+#
+# Meant to configure a VIMRC file for Ansible or OCP Courses
+
+echo "Cloning Repository"
+git clone https://github.com/tmichett/vimrc.git rht_vimrc
+
+echo "Executing VIMRC Configuration Playbook"
+cd ~/rht_vimrc
+ansible-playbook VIMRC_Setup.yml
+
+
